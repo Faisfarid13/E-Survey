8f9265ac0dc4fc154c3c02fa7bef3b04d087aa73
citraregita <citraregitanws@gmail.com>
2023-09-25 12:47:33 +0700

    add validation field

--- app/Filament/Resources/SurveyResource/RelationManagers/QuestionsRelationManager.php
+++ app/Filament/Resources/SurveyResource/RelationManagers/QuestionsRelationManager.php
@@ -3,10 +3,14 @@
 namespace App\Filament\Resources\SurveyResource\RelationManagers;
 
 use Filament\Forms;
+use Filament\Forms\Components\Grid;
 use Filament\Forms\Form;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
 use Filament\Tables\Table;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Get;
+use Filament\Forms\Set;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
@@ -22,7 +26,61 @@ class QuestionsRelationManager extends RelationManager
                     ->required()
                     ->maxLength(255),
                 Forms\Components\Select::make('question_category_id')
-                    ->relationship('question_category', 'type')
+                    ->relationship('question_category', 'type'),
+                Forms\Components\Section::make('Validasi')
+                    ->schema([
+                        Forms\Components\Repeater::make('validation')
+                        ->schema([
+                            Forms\Components\Select::make('type')
+                                ->label('Jenis')
+                                ->options([
+                                    'required' => 'Wajib Diisi',
+                                    'numeric' => 'Jawaban Hanya Berisi Angka',
+                                    'string' => 'Jawaban Bisa Berisi Apa Saja',
+                                    'minLength' => 'Huruf Minimal',
+                                    'maxLength' => 'Huruf Maksimal',
+                                ])
+                                ->required()
+                                ->searchable()
+                                ->live()
+                                // ->afterStateUpdated(fn (Select $component) => $component
+                                // ->getContainer()
+                                // ->getComponent('dynamicTypeFields')
+                                // ->getChildComponentContainer()
+                                // ->fill())
+                                //     [
+                                // Grid::make(2)
+                                // ->schema(fn (Get $get): array => match ($get('type')) {
+                                //     'minLength' => [
+                                //         Forms\Components\TextInput::make('value')
+                                //             ->required(),
+                                //     ],
+                                //     'maxLength' => [
+                                //         Forms\Components\TextInput::make('value')
+                                //             ->required(),
+                                //     ],
+                                //     default => [],
+                                // })
+                                // ->key('dynamicTypeFields')]
+                                
+                                
+                             
+                            // Forms\Components\TextInput::make('value')
+                            //     ->hidden(function (Get $get) 
+                            // {
+                            //     if (in_array($get['validation'],['required','numeric','string']))
+                            //     {
+                            //         true;
+                            //     } else{
+                            //         false;
+                            //     }
+                            
+                            // }),
+                        ])
+                        
+                        ->columns(1)
+                    ]),
+
             ]);
     }
     public function table(Table $table): Table
@@ -52,3 +110,12 @@ class QuestionsRelationManager extends RelationManager
             ]);
     }
 }
+
+// if (in_array($state,['required','numeric','string']))
+// {
+//     $set('value', hidden(true));
+// }
+// else{
+//     $set('value')->hidden(false);
+// }
+// }
--- app/Models/Question.php
+++ app/Models/Question.php
@@ -14,6 +14,7 @@ class Question extends Model
     protected $guarded = [
         'id'
     ];
+    protected $casts = ['validation'=> 'array'];
     public function survey()
     {
         return $this->belongsTo(Survey::class);
